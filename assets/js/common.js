76835cb0567359bf95b26f32b2aea2ec1d188f0d
kimkyungm13 <kyung_m13@naver.com>
2024-03-04 17:51:42 +0900

    1

---
+++ assets/js/common.js
@@ -0,0 +1,12 @@
+$(function () {
+    const topSwiper = new Swiper(".top-banner", {
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+    });
+});
