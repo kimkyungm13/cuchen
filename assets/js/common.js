7aa3e889c61dcba9a42b5d395ea99af48508ad1c
kimkyungm13 <kyung_m13@naver.com>
2024-03-05 08:50:27 +0900

    1

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,21 @@
 $(function () {
     const topSwiper = new Swiper(".top-banner", {
-        effect: 'fade',
-        fadeEffect: {
-            crossFade: true
-        },
+        direction: "vertical",
         autoplay: {
             delay: 3000,
             disableOnInteraction: false,
+        },
+    });
+    const mainSwiper = new Swiper(".mainSwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+                var pageIndex = index + 1;
+                var bulletText = pageIndex >= 10 ? pageIndex.toString() : '0' + pageIndex;
+                return '<span class="' + className + '">' + bulletText + '</span>';
+
+            },
         },
     });
 });
